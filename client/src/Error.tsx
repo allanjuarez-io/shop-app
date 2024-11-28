a8ab9062e9b5fd72f911990fe5a1d149e23af329
Allan <180233210+allanjuarez-io@users.noreply.github.com>
2024-11-27 19:02:01 -0600

    Fixed: component Error

--- client/src/Error.tsx
+++ client/src/Error.tsx
@@ -1,12 +1,13 @@
 import { useRouteError } from 'react-router';
 
 export default function Error() {
-  const error = useRouteError();
+  const error = useRouteError() as Error;
+
   return (
     <div>
       <h4>Algo no salio bien aqui.</h4>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error.message}</i>
       </p>
     </div>
   );
